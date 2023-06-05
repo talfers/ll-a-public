5084d90d258fbb0d340cdce8454efbe6a471f7c0
Taylor Alfers <tayloralfers@gmail.com>
2023-06-04 19:50:12 -0600

    setup for env

--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,13 @@
 // import keys from '../keys'
 import db, { provider, analytics, auth } from './firebase'
+import env from "@beam-australia/react-env";
 
 const config = { REACT_APP_OPENAI_API_KEY: '', REACT_APP_OPENAI_URL: '', db, provider, analytics, auth, REACT_APP_RECAPTCHA_SITE_KEY: '', REACT_APP_RECAPTCHA_SECRET_KEY: '' }
 // if (process.env.REACT_APP_ENV === 'PROD') {
-config.REACT_APP_OPENAI_API_KEY = process.env.OPENAI_API_KEY
-config.REACT_APP_OPENAI_URL = process.env.OPENAI_URL
-config.REACT_APP_RECAPTCHA_SITE_KEY = process.env.RECAPTCHA_SITE_KEY
-config.REACT_APP_RECAPTCHA_SECRET_KEY = process.env.RECAPTCHA_SECRET_KEY
+config.REACT_APP_OPENAI_API_KEY = env("OPENAI_API_KEY")
+config.REACT_APP_OPENAI_URL = env("OPENAI_URL")
+config.REACT_APP_RECAPTCHA_SITE_KEY = env("RECAPTCHA_SITE_KEY")
+config.REACT_APP_RECAPTCHA_SECRET_KEY = env("RECAPTCHA_SECRET_KEY")
 // } 
 // else {
 //     config.REACT_APP_OPENAI_API_KEY = keys.REACT_APP_OPENAI_API_KEY
