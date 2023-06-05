896e138de11c0449d9fe2d56b9c220c5f75aaeeb
Taylor Alfers <tayloralfers@gmail.com>
2023-06-04 19:31:36 -0600

    env test

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+// import env from "@beam-australia/react-env";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -10,7 +11,7 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "landlordassist.io",
   projectId: "landlord-assistant",
   storageBucket: "landlord-assistant.appspot.com",
--- src/config/index.js
+++ src/config/index.js
@@ -3,10 +3,10 @@ import db, { provider, analytics, auth } from './firebase'
 
 const config = { REACT_APP_OPENAI_API_KEY: '', REACT_APP_OPENAI_URL: '', db, provider, analytics, auth, REACT_APP_RECAPTCHA_SITE_KEY: '', REACT_APP_RECAPTCHA_SECRET_KEY: '' }
 // if (process.env.REACT_APP_ENV === 'PROD') {
-config.REACT_APP_OPENAI_API_KEY = process.env.REACT_APP_OPENAI_API_KEY
-config.REACT_APP_OPENAI_URL = process.env.REACT_APP_OPENAI_URL
-config.REACT_APP_RECAPTCHA_SITE_KEY = process.env.REACT_APP_RECAPTCHA_SITE_KEY
-config.REACT_APP_RECAPTCHA_SECRET_KEY = process.env.REACT_APP_RECAPTCHA_SECRET_KEY
+config.REACT_APP_OPENAI_API_KEY = process.env.OPENAI_API_KEY
+config.REACT_APP_OPENAI_URL = process.env.OPENAI_URL
+config.REACT_APP_RECAPTCHA_SITE_KEY = process.env.RECAPTCHA_SITE_KEY
+config.REACT_APP_RECAPTCHA_SECRET_KEY = process.env.RECAPTCHA_SECRET_KEY
 // } 
 // else {
 //     config.REACT_APP_OPENAI_API_KEY = keys.REACT_APP_OPENAI_API_KEY
