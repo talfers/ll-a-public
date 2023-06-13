a91366a35e0ee519e43fd4188d7987b8c79f407d
talfer220_comcast <Taylor_Alfers@comcast.com>
2023-06-12 22:08:54 -0600

    setup manage and profile style

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -44,10 +44,16 @@ const Profile = () => {
                     <ProfileHeaderStyled>{user.email}</ProfileHeaderStyled>
                 </ProfileHeaderContainerStyled>
                 
-                <ProfileTextStyled>Status: <span style={{color: subscription?.status==='active'?'green':'red'}}>{subscription?.status.charAt(0).toUpperCase() + subscription?.status.slice(1)}</span></ProfileTextStyled>
-                <ProfileTextStyled>Plan: ${subscription?.plan.price.unit_amount/100} / {subscription?.plan.plan.interval}</ProfileTextStyled>
-                <ProfileTextStyled>Member since: {subscription?.current_period_start_date}</ProfileTextStyled>
-                <ProfileTextStyled>Renewal date: {subscription?.current_period_end_date}</ProfileTextStyled>
+                {   subscription?
+                    <>
+                        <ProfileTextStyled>Status: <span style={{color: subscription?.status==='active'?'green':'red'}}>{subscription?.status.charAt(0).toUpperCase() + subscription?.status.slice(1)}</span></ProfileTextStyled>
+                        <ProfileTextStyled>Plan: ${subscription?.plan.price.unit_amount/100} / {subscription?.plan.plan.interval}</ProfileTextStyled>
+                        <ProfileTextStyled>Member since: {subscription?.current_period_start_date}</ProfileTextStyled>
+                        <ProfileTextStyled>Renewal date: {subscription?.current_period_end_date}</ProfileTextStyled>
+                    </>:
+                    <div>loading...</div>
+                    
+                }
             </ProfileContentContainerStyled>
             <ActionButtonStyled onClick={() => manageSubscription(customer.stripeId)}>Manage Account</ActionButtonStyled>
             <ActionButtonStyled onClick={() => logOut()}>Signout</ActionButtonStyled>
